c3230a308a548a4c11c7e42571df6906cf080a05
Robinhuett <5955614+Robinhuett@users.noreply.github.com>
2018-11-07 20:48:08 +0100

    Updates & rice

--- .local/bin/scripts/lock.sh
+++ .local/bin/scripts/lock.sh
@@ -10,7 +10,7 @@ for output in $(swaymsg -t get_outputs | jq -r '.[] .name'); do
   grim -o $output $image
 done
 
-printf '%s\n' "${images[@]}" | xargs -P 0 -I{} convert -scale 20% -blur 8x3 -scale 500% {} {}
+printf '%s\n' "${images[@]}" | xargs -P 0 -I{} convert -blur 0x8 {} {}
 
 swaylock ${swaylock_args[@]} -s center
 rm ${images[@]}
